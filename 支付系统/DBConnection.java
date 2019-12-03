ef899e35a68597a32af820fbf21cfab455f769da
hzy1740610921 <55789879+hzy1740610921@users.noreply.github.com>
2019-12-03 14:12:09 +0800

    Add files via upload

---
+++ 支付系统/DBConnection.java
@@ -0,0 +1,113 @@
+package Item1;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+public class DBConnection {
+	public static Connection con=null;
+	
+	static{
+		String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+		String url="jdbc:sqlserver://localhost:1433;DatabaseName=asd";
+		String user="sa",password="123456";
+		try {
+			Class.forName(driver);
+			con=DriverManager.getConnection(url, user, password);
+			System.out.println("数据库连接成功!");
+		}catch(ClassNotFoundException e) {
+			System.out.println("驱动程序装载失败!");
+		}
+		catch(SQLException e) {
+			System.out.println("数据库连接失败!");
+		}
+	}
+	
+	public int query(String username , String password) {
+		int result = -1;
+		try {
+			Statement sql = con.createStatement();
+			String sqlString = "select * from asd.dbo.login where 1=1 and card = '"+username+"' and password = '"+password+"';";
+			ResultSet rs = sql.executeQuery(sqlString);
+			
+			while(rs.next()) {
+				result++;
+				JOptionPane.showMessageDialog(null, "登陆成功！","消息对话框",JOptionPane.DEFAULT_OPTION); 
+			}
+			
+			if(result == -1)
+				JOptionPane.showMessageDialog(null, "账号或密码错误 !","消息对话框",JOptionPane.WARNING_MESSAGE);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public int queryb(float balance) {
+		int result = -1;
+		try {
+			Statement sql = con.createStatement();
+			String sqlString = "select * from asd.dbo.login where 1=1 and card = "+balance;
+			ResultSet rs = sql.executeQuery(sqlString);
+			
+			while(rs.next()) {
+				result++;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	/**
+	 * 通过账号查询用户信息
+	 * @param cardNumber
+	 * @return
+	 */
+	public BankCard queryC(String card) {
+		int result = -1;
+		BankCard bankCard = new BankCard();
+		try {
+			Statement sql = con.createStatement();
+			String sqlString = "select * from asd.dbo.login where 1=1 and card = "+card;
+			ResultSet rs = sql.executeQuery(sqlString);
+			while(rs.next()){
+				bankCard.OTime = rs.getString("opentime");
+				bankCard.name = rs.getString("username");
+				bankCard.card = rs.getInt("card");
+				bankCard.balance = rs.getFloat("balance");
+				bankCard.password = rs.getString("password");
+			}
+			
+			while(rs.next()) {
+				result++;
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return bankCard;
+	}
+	
+	public int updata(float balance,int card) {
+		int result=-1;
+		try {
+			Statement sql = con.createStatement();
+			String sqlString = "update asd.dbo.login set balance = "+balance+"where card = '"+card+"';";
+			System.out.println(sqlString);
+			sql.executeUpdate(sqlString);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return  result;
+	}
+	
+	public static void main(String[] args) {
+		DBConnection dBConnection = new DBConnection();
+		dBConnection.query("刘晨", "123456");
+	}
+}
